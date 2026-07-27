3f564f73d4702f7e8645964473eaf58d92c71196
mudassirrayeen <mudassirrayeen521@gmail.com>
2026-07-27 18:03:37 +0530

    Time: 8 ms (44.57%), Space: 47.3 MB (33.36%) - LeetHub

--- 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
+++ 1019-next-greater-node-in-linked-list/1019-next-greater-node-in-linked-list.cpp
@@ -1,36 +1,25 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     vector<int> nextLargerNodes(ListNode* head) {
-       vector<int> ll;
+        vector<int> nums;
 
-while(head){
-    ll.push_back(head->val);
-    head = head->next;
-}
+        while (head) {
+            nums.push_back(head->val);
+            head = head->next;
+        }
 
-vector<int> ans(ll.size(), 0);
-stack<int> st;
+        int n = nums.size();
+        vector<int> ans(n, 0);
+        stack<int> st; // Stores indices
 
-for(int i = 0; i < ll.size(); i++){
+        for (int i = 0; i < n; i++) {
+            while (!st.empty() && nums[st.top()] < nums[i]) {
+                ans[st.top()] = nums[i];
+                st.pop();
+            }
+            st.push(i);
+        }
 
-    while(!st.empty() && ll[i] > ll[st.top()]){
-        ans[st.top()] = ll[i];
-        st.pop();
-    }
-
-    st.push(i);
-}
-
-return ans;
+        return ans;
     }
 };
