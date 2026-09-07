603d2c5dfca559a1f8ac399ac7d31c0aee5548cb
Mudassir Rayeen <mudassirrayeen521@gmail.com>
2026-09-07 19:19:51 +0530

    Time: 19 ms (41.26%), Space: 22.1 MB (30.44%) - LeetHub

---
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    string longestPalindrome(string s) {
+        string ans = "";
+
+        for (int i = 0; i < s.length(); i++) {
+
+            // Odd length palindrome
+            int left = i;
+            int right = i;
+
+            while (left >= 0 && right < s.length() &&
+                   s[left] == s[right]) {
+
+                if (right - left + 1 > ans.length())
+                    ans = s.substr(left, right - left + 1);
+
+                left--;
+                right++;
+            }
+
+            // Even length palindrome
+            left = i;
+            right = i + 1;
+
+            while (left >= 0 && right < s.length() &&
+                   s[left] == s[right]) {
+
+                if (right - left + 1 > ans.length())
+                    ans = s.substr(left, right - left + 1);
+
+                left--;
+                right++;
+            }
+        }
+
+        return ans;
+    }
+};
